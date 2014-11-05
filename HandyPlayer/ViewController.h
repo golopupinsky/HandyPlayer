d945b08905fae0b68b2973c0046b08aca80b3e6f
Sergey Yuzepovich <yuzepovich@gmail.com>
2014-11-05 17:49:52 +0300

    Initial Commit

---
+++ HandyPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HandyPlayer
+//
+//  Created by Sergey Yuzepovich on 05.11.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ HandyPlayer/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  HandyPlayer
+//
+//  Created by Sergey Yuzepovich on 05.11.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+@end
